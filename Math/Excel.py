682f7683f430ecb9cdd17609b054e68573163827
Arunima Chatterjee <92734728+TheHappyBaloney@users.noreply.github.com>
2024-07-28 11:16:47 +0530

    168. Excel Sheet Column Title

---
+++ Math/Excel.py
@@ -0,0 +1,11 @@
+# 168. Excel Sheet Column Title
+
+class Solution:
+    def convertToTitle(self, columnNumber: int) -> str:
+        ans = ""
+        while columnNumber > 0:
+            columnNumber -= 1
+            ans += chr(columnNumber % 26 + ord("A"))
+            columnNumber //= 26
+
+        return ans[::-1]
